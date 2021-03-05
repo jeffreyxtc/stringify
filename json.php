21fa2de4aba32b118fd468af724cf2d597bf5e5c
Jeffrey Wright <jeffreyxtc@hotmail.com>
2021-03-05 19:40:54 +0000

    Initial commit

---
+++ json.php
@@ -0,0 +1,29 @@
+<?php
+$v = '{ 
+
+   "import_set": "", 
+   "staging_table": "", 
+   "result":    [ 
+            { 
+         "transform_map": "", 
+         "table": "", 
+         "display_name": "", 
+         "display_value": "", 
+         "record_link": "", 
+         "status": "inserted", 
+         "sys_id": "" 
+      }, 
+            { 
+         "transform_map": "", 
+         "table": "", 
+         "display_name": "", 
+         "display_value": "", 
+         "record_link": "", 
+         "status": "inserted", 
+         "sys_id": "" 
+      } 
+   ] 
+}';
+
+echo $v;
+?>
