c8e24b41342b02ead379cdd805d9eb76fc66de0a
Jeffrey Wright <jeffreyxtc@hotmail.com>
2021-03-06 12:34:52 +0000

    Refinement

--- json.php
+++ json.php
@@ -2,34 +2,32 @@
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
-$v = array (
-  'import_set' => '',
-  'staging_table' => '',
-  'result' => 
-  array (
-    0 => 
-    array (
-      'transform_map' => '',
-      'table' => '',
-      'display_name' => '',
-      'display_value' => '',
-      'record_link' => '',
-      'status' => 'inserted',
-      'sys_id' => '',
-    ),
-    1 => 
-    array (
-      'transform_map' => '',
-      'table' => '',
-      'display_name' => '',
-      'display_value' => '',
-      'record_link' => '',
-      'status' => 'inserted',
-      'sys_id' => '',
-    ),
-  ),
-);
-
-echo json_encode($v);
-exit;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $v = array(
+        'import_set' => '',
+        'staging_table' => '',
+        'result' => array(
+            0 => array(
+                'transform_map' => '',
+                'table' => '',
+                'display_name' => '',
+                'display_value' => '',
+                'record_link' => '',
+                'status' => 'inserted',
+                'sys_id' => ''
+            ),
+            1 => array(
+                'transform_map' => '',
+                'table' => '',
+                'display_name' => '',
+                'display_value' => '',
+                'record_link' => '',
+                'status' => 'inserted',
+                'sys_id' => ''
+            )
+        )
+    ); 
+    echo json_encode($v);
+    exit;
+}
 ?>
